6878112568797d7bee22facfd5f4e70b2c814531
jerrytse-oss <jerry_tse_alt@hotmail.com>
2026-03-05 12:26:36 +0800

    add: add Realme EDL ROM query
    * Check usage for get how to use, lazy to explain

---
+++ realme_edl_query.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Designed by Jerry Tse
+import sys
+import re
+import requests
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+def check_url(url):
+    try:
+        resp = requests.head(url, timeout=2, allow_redirects=True)
+        if resp.status_code == 200:
+            print("Fetch Info:")
+            print(f"• Link: {url}")
+            os._exit(0) 
+    except:
+        pass
+
+def main():
+    if len(sys.argv) != 5:
+        print("Usage: python3 script.py <MODEL> <REGION> <VERSION_NAME> <DATE>")
+        print("Example: ~/venv/bin/python realme_edl_query.py RMX3888 CN \"RMX3888_16.0.3.500(CN01)\" 202601241320")
+        return
+
+    MODEL = sys.argv[1]
+    REGION = sys.argv[2].upper()
+    VERSION_NAME = sys.argv[3]
+    DATE_PREFIX = sys.argv[4]
+    
+    if len(DATE_PREFIX) != 12:
+        print(f"\n❌ Error: Argument 4 (Date) length is {len(DATE_PREFIX)}, expected 12 characters.")
+        sys.exit(1)
+
+    if REGION in ("EU", "EUEX", "EEA", "TR"):
+        BUCKET, SERVER = "GDPR", "rms01.realme.net"
+    elif REGION in ("CN", "CH"):
+        BUCKET, SERVER = "domestic", "rms11.realme.net"
+    else:
+        BUCKET, SERVER = "export", "rms01.realme.net"
+
+    VERSION_CLEAN = re.sub(r"^RMX\d+_", "", VERSION_NAME).replace("(", "").replace(")", "")
+    BASE_URL = f"https://{SERVER}/sw/{MODEL}{BUCKET}_11_{VERSION_CLEAN}_{DATE_PREFIX}"
+
+    print(f"Querying for {VERSION_NAME}\n")
+
+    executor = ThreadPoolExecutor(max_workers=100)
+
+    try:
+        for i in range(10000):
+            url = f"{BASE_URL}{i:04d}.zip"
+            executor.submit(check_url, url)
+            
+        executor.shutdown(wait=True)
+    except KeyboardInterrupt:
+        os._exit(1)
+
+    print("Fetch Info:")
+    print("• Link: Not Found")
+
+if __name__ == "__main__":
+    main()
